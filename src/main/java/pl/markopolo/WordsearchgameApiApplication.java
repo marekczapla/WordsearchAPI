8988d23e45a8bdb056f4d6347024606937a83762
marekczapla <106765325+marekczapla@users.noreply.github.com>
2022-10-28 13:00:47 +0200

    Wordsearch API implementation

---
+++ src/main/java/pl/markopolo/WordsearchgameApiApplication.java
@@ -0,0 +1,13 @@
+package pl.markopolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordsearchgameApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordsearchgameApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pl/markopolo/controllers/WorldSearchGameController.java
@@ -0,0 +1,30 @@
+package pl.markopolo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.markopolo.services.WordGridService;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@RestController("/")
+public class WorldSearchGameController {
+
+    @Autowired
+    WordGridService wordGridService;
+
+    @GetMapping("/wordgrid")
+    public String createWordGrid(@RequestParam int gridSize, @RequestParam(required=false) String wordList) {
+        List<String> words = Arrays.asList(wordList.split(","));
+        char[][] grid = wordGridService.generateGrid(gridSize, words);
+        String gridToString = "";
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                gridToString += grid[i][j] + " ";
+            }
+            gridToString += "\r\n";
+        }
+        return gridToString;
+    }
+}
